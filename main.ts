182887b678b526f83774a1d3c7db4470040b0e54
Satvik-web <72124172+Satvik-web@users.noreply.github.com>
2020-10-24 14:07:18 +0530

    Update pxt.json, main.blocks, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
-basic.forever(function () {
-	
+enum RadioMessage {
+    message1 = 49434
+}
+input.onButtonPressed(Button.A, function () {
+    music.playMelody("E E F G G F E D ", 120)
+    music.playMelody("C C D E E D D - ", 120)
+    music.playMelody("E E F G G F E D ", 120)
+    music.playMelody("C C D E D C C - ", 120)
+    basic.showString("Press The Button B to Hear The Full Song")
 })
+input.onButtonPressed(Button.B, function () {
+    radio.sendValue("Play Ode To Joy Fully", 0)
+})
+basic.showString("Press The Button A To Start Playing")
