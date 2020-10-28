1ec86fc0f9fce4860982f7f14bc1534ee61bc2b1
Satvik-web <72124172+Satvik-web@users.noreply.github.com>
2020-10-28 11:43:00 +0530

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
 enum RadioMessage {
     message1 = 49434
 }
+let Degree = 0
+basic.forever(function () {
+    Degree = input.compassHeading()
+    if (Degree < 45) {
+        basic.showString("N")
+    } else if (Degree < 135) {
+        basic.showString("E")
+    } else if (Degree < 225) {
+        basic.showString("S")
+    } else if (Degree < 315) {
+        basic.showString("W")
+    } else {
+        basic.showString("N")
+    }
+})
